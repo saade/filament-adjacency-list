2d267d4ed90fdf3caa2d0bb350e25360c000db0d
Sander <51762658+vanhooff@users.noreply.github.com>
2023-09-13 16:14:56 +0200

    Added maxDepth index.js

--- resources/js/index.js
+++ resources/js/index.js
@@ -2,10 +2,12 @@ import Sortable from "sortablejs"
 
 export default ({
     statePath,
-    disabled
+    disabled,
+    maxDepth
 }) => ({
     statePath,
     sortable: null,
+    maxDepth,
 
     init() {
         this.sortable = new Sortable(this.$el, {
@@ -16,9 +18,25 @@ export default ({
             swapThreshold: 0.50,
             draggable: "[data-sortable-item]",
             handle: "[data-sortable-handle]",
-            onSort: () => {
+            onSort: (evt) => {
+
+                if (this.maxDepth !== null && this.getDepth(evt.item) > this.maxDepth) {
+                    // Undo the sorting
+                    evt.from.insertBefore(evt.item, evt.pullMode ? null : evt.from.children[evt.oldIndex]);
+                }
+
                 this.$wire.dispatchFormEvent('builder::sort', this.statePath, this.sortable.toArray())
             }
         })
-    }
- })
+    },
+
+    getDepth(el, depth = 0) {
+        let parentEl = el.parentElement.closest('[data-sortable-item]');
+        if (parentEl) {
+            return this.getDepth(parentEl, ++depth);
+        }
+        return depth;
+    },
+
+
+})
