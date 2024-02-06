d8fdfaa468c13313b65bcfd433dc945da084d2b6
saade <saade@users.noreply.github.com>
2024-02-06 12:55:24 +0000

    Fix styling

--- src/Forms/Components/Concerns/HasRelationships.php
+++ src/Forms/Components/Concerns/HasRelationships.php
@@ -45,7 +45,7 @@ trait HasRelationships
         return $this;
     }
 
-    public function relationship(string | Closure $name = null, Closure $modifyQueryUsing = null): static
+    public function relationship(string | Closure | null $name = null, ?Closure $modifyQueryUsing = null): static
     {
         $this->relationship = $name ?? $this->getName();
         $this->modifyRelationshipQueryUsing = $modifyQueryUsing;
