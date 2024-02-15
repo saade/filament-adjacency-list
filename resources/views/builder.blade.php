475ddcf8001fb1f9555364b77b7a2e1380c261b8
Saade <saade@outlook.com.br>
2024-02-15 10:11:59 -0300

    refactor: html markup

--- resources/views/builder.blade.php
+++ resources/views/builder.blade.php
@@ -1,5 +1,5 @@
 <x-filament-forms::field-wrapper
-    class="filament-navigation"
+    class="fi-adjacency-list-wrapper"
     :id="$getId()"
     :label="$getLabel()"
     :label-sr-only="$isLabelHidden()"
@@ -29,54 +29,52 @@
         $itemActions = [$getAction('addChild'), $getAction('delete'), $getAction('edit'), $getAction('reorder'), $getAction('indent'), $getAction('dedent'), $getAction('moveUp'), $getAction('moveDown')];
     @endphp
 
-    <div wire:key="tree-items-wrapper">
-        <div
-            data-sortable-container
-            ax-load
-            ax-load-css="{{ \Filament\Support\Facades\FilamentAsset::getStyleHref('filament-adjacency-list-styles', 'saade/filament-adjacency-list') }}"
-            ax-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('filament-adjacency-list', 'saade/filament-adjacency-list') }}"
-            x-data="tree({
-                treeId: @js($treeId),
-                statePath: @js($getStatePath()),
-                disabled: @js($isDisabled),
-                maxDepth: @js($maxDepth)
-            })"
-        >
-            @forelse($getState() as $uuid => $item)
-                <x-filament-adjacency-list::item
-                    :actions="$itemActions"
-                    :addable="$isAddable"
-                    :ascendable="$isMoveable && !$loop->first"
-                    :children-key="$getChildrenKey()"
-                    :dedentable="$isIndentable && false"
-                    :deletable="$isDeletable"
-                    :descendable="$isMoveable && !$loop->last"
-                    :disabled="$isDisabled"
-                    :editable="$isEditable"
-                    :has-rulers="$hasRulers"
-                    :indentable="$isIndentable && (!$loop->first && $loop->count > 1)"
-                    :is-collapsed="$isCollapsed"
-                    :is-collapsible="$isCollapsible"
-                    :is-indentable="$isIndentable"
-                    :is-moveable="$isMoveable"
-                    :item="$item"
-                    :item-state-path="$getStatePath() . '.' . $uuid"
-                    :label-key="$getLabelKey()"
-                    :max-depth="$maxDepth"
-                    :reorderable="$isReorderable"
-                    :state-path="$getStatePath()"
-                    :tree-id="$treeId"
-                    :uuid="$uuid"
-                />
-            @empty
-                <div @class([
-                    'w-full bg-white rounded-lg border border-gray-300 px-3 py-2 text-left rtl:text-right',
-                    'dark:bg-gray-900 dark:border-white/10',
-                ])>
-                    {{ __('filament-adjacency-list::adjacency-list.items.empty') }}
-                </div>
-            @endforelse
-        </div>
+    <div
+        class="fi-adjacency-list-tree"
+        data-sortable-container
+        ax-load
+        ax-load-css="{{ \Filament\Support\Facades\FilamentAsset::getStyleHref('filament-adjacency-list-styles', 'saade/filament-adjacency-list') }}"
+        ax-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('filament-adjacency-list', 'saade/filament-adjacency-list') }}"
+        x-data="tree({
+            treeId: @js($treeId),
+            statePath: @js($getStatePath()),
+            disabled: @js($isDisabled),
+            maxDepth: @js($maxDepth)
+        })"
+    >
+        @forelse($getState() as $uuid => $item)
+            <x-filament-adjacency-list::item
+                class="fi-adjacency-list-root"
+                :actions="$itemActions"
+                :addable="$isAddable"
+                :ascendable="$isMoveable && !$loop->first"
+                :children-key="$getChildrenKey()"
+                :dedentable="$isIndentable && false"
+                :deletable="$isDeletable"
+                :descendable="$isMoveable && !$loop->last"
+                :disabled="$isDisabled"
+                :editable="$isEditable"
+                :has-rulers="$hasRulers"
+                :indentable="$isIndentable && (!$loop->first && $loop->count > 1)"
+                :is-collapsed="$isCollapsed"
+                :is-collapsible="$isCollapsible"
+                :is-indentable="$isIndentable"
+                :is-moveable="$isMoveable"
+                :item="$item"
+                :item-state-path="$getStatePath() . '.' . $uuid"
+                :label-key="$getLabelKey()"
+                :max-depth="$maxDepth"
+                :reorderable="$isReorderable"
+                :state-path="$getStatePath()"
+                :tree-id="$treeId"
+                :uuid="$uuid"
+            />
+        @empty
+            <div
+                class="w-full px-3 py-2 text-left bg-white border border-gray-300 rounded-lg rtl:text-right dark:bg-gray-900 dark:border-white/10">
+                {{ __('filament-adjacency-list::adjacency-list.items.empty') }}
+            </div>
+        @endforelse
     </div>
 
     <div class="flex justify-end">
